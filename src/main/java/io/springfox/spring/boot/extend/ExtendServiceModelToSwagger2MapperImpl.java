fa40e47f2d067ab5dd7d7d1a1408c3ac651ac2dd
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 23:55:33 +0800

    refactor: namespace hiwepy→easy4j (copyright 残留)

--- src/main/java/io/springfox/spring/boot/extend/ExtendServiceModelToSwagger2MapperImpl.java
+++ src/main/java/io/springfox/spring/boot/extend/ExtendServiceModelToSwagger2MapperImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, hiwepy (https://github.com/easy-4-java).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -41,7 +41,7 @@ import springfox.documentation.swagger2.mappers.ServiceModelToSwagger2MapperImpl
 /**
  * TODO
  * 
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author ： <a href="https://github.com/easy-4-java">hiwepy</a>
  */
 @Slf4j
 public class ExtendServiceModelToSwagger2MapperImpl extends ServiceModelToSwagger2MapperImpl {
